cdff5bf395f8d9da183744909f015e29e50931a7
Egor Kromberg <pomidorka.kromberg@gmail.com>
2015-10-01 23:02:30 +0300

    1002 v2

--- timus/timus/1002.cpp
+++ timus/timus/1002.cpp
@@ -3,36 +3,36 @@
 #include <list>
 #include <string>
 
-static char aLettersNumbersMap[int32_t('z')] = { 0 };
+static char aLettersNumbersMap[int32_t('z') - int32_t('a') + 1] = { 0 };
 
 void _FormLettersNumbersMap()
 {
-    aLettersNumbersMap['i'] = '1';
-    aLettersNumbersMap['j'] = '1';
-    aLettersNumbersMap['a'] = '2';
-    aLettersNumbersMap['b'] = '2';
-    aLettersNumbersMap['c'] = '2';
-    aLettersNumbersMap['d'] = '3';
-    aLettersNumbersMap['e'] = '3';
-    aLettersNumbersMap['f'] = '3';
-    aLettersNumbersMap['g'] = '4';
-    aLettersNumbersMap['h'] = '4';
-    aLettersNumbersMap['k'] = '5';
-    aLettersNumbersMap['l'] = '5';
-    aLettersNumbersMap['m'] = '6';
-    aLettersNumbersMap['n'] = '6';
-    aLettersNumbersMap['p'] = '7';
-    aLettersNumbersMap['r'] = '7';
-    aLettersNumbersMap['s'] = '7';
-    aLettersNumbersMap['t'] = '8';
-    aLettersNumbersMap['u'] = '8';
-    aLettersNumbersMap['v'] = '8';
-    aLettersNumbersMap['w'] = '9';
-    aLettersNumbersMap['x'] = '9';
-    aLettersNumbersMap['y'] = '9';
-    aLettersNumbersMap['o'] = '0';
-    aLettersNumbersMap['q'] = '0';
-    aLettersNumbersMap['z'] = '0';
+    aLettersNumbersMap['i' - 'a'] = '1';
+    aLettersNumbersMap['j' - 'a'] = '1';
+    aLettersNumbersMap['a' - 'a'] = '2';
+    aLettersNumbersMap['b' - 'a'] = '2';
+    aLettersNumbersMap['c' - 'a'] = '2';
+    aLettersNumbersMap['d' - 'a'] = '3';
+    aLettersNumbersMap['e' - 'a'] = '3';
+    aLettersNumbersMap['f' - 'a'] = '3';
+    aLettersNumbersMap['g' - 'a'] = '4';
+    aLettersNumbersMap['h' - 'a'] = '4';
+    aLettersNumbersMap['k' - 'a'] = '5';
+    aLettersNumbersMap['l' - 'a'] = '5';
+    aLettersNumbersMap['m' - 'a'] = '6';
+    aLettersNumbersMap['n' - 'a'] = '6';
+    aLettersNumbersMap['p' - 'a'] = '7';
+    aLettersNumbersMap['r' - 'a'] = '7';
+    aLettersNumbersMap['s' - 'a'] = '7';
+    aLettersNumbersMap['t' - 'a'] = '8';
+    aLettersNumbersMap['u' - 'a'] = '8';
+    aLettersNumbersMap['v' - 'a'] = '8';
+    aLettersNumbersMap['w' - 'a'] = '9';
+    aLettersNumbersMap['x' - 'a'] = '9';
+    aLettersNumbersMap['y' - 'a'] = '9';
+    aLettersNumbersMap['o' - 'a'] = '0';
+    aLettersNumbersMap['q' - 'a'] = '0';
+    aLettersNumbersMap['z' - 'a'] = '0';
 }
 
 template <typename T>
@@ -43,19 +43,21 @@ inline T min(T& t1, T& t2)
 
 struct Number
 {
-    bool m_bUsed;
     int32_t m_i32NumberSize;
     char* m_szNumber;
+    char* m_szWord;
 };
 
 void _FormNumber(const char* szWord, Number &number)
 {
     number.m_i32NumberSize = strlen(szWord);
     number.m_szNumber = new char[number.m_i32NumberSize + 1];
+    number.m_szWord = new char[number.m_i32NumberSize + 1];
+    strcpy(number.m_szWord, szWord);
     char *sTmpNumber = number.m_szNumber;
-    while (*szWord++)
+    while (*szWord)
     {
-        *sTmpNumber++ = aLettersNumbersMap[*szWord];
+        *sTmpNumber++ = aLettersNumbersMap[(*szWord++) - 'a'];
     }
     *sTmpNumber = '\0';
 }
@@ -67,41 +69,10 @@ bool _RecursiveGetSolution(
     Number* pNumbers,
     std::list<Number> &lNumbers)
 {
-    std::list<Number> lMinNumbers;
-    for (int32_t i = 0; i < i32NumbersCount; ++i)
+    if (!(*szTelephoneNumber))
     {
-        std::list<Number> lTmpNumbers;
-        if (!pNumbers[i].m_bUsed &&
-            (i32TelephoneNumberSize >= pNumbers[i].m_i32NumberSize) &&
-            !strncmp(szTelephoneNumber, pNumbers[i].m_szNumber, pNumbers[i].m_i32NumberSize))
-        {
-            pNumbers[i].m_bUsed = true;
-            bool result = _RecursiveGetSolution(
-                szTelephoneNumber + pNumbers[i].m_i32NumberSize,
-                i32TelephoneNumberSize - pNumbers[i].m_i32NumberSize,
-                i32NumbersCount,
-                pNumbers,
-                lTmpNumbers);
-
-            if (result)
-            {
-                if (lMinNumbers.empty() || lMinNumbers.size() > lTmpNumbers.size())
-                {
-                    lMinNumbers = lTmpNumbers;
-                }
-            }
-            pNumbers[i].m_bUsed = false;
-        }
+        return true;
     }
-    return lMinNumbers.empty() ? false : true;
-}
-
-std::list<Number> _GetSolution(char* szTelephoneNumber, int32_t i32NumbersCount, Number* pNumbers)
-{
-    static Number noSolution = { false, 12, "No solution." };
-
-    int32_t i32TelephoneNumberSize = strlen(szTelephoneNumber);
-
     std::list<Number> lMinNumbers;
     for (int32_t i = 0; i < i32NumbersCount; ++i)
     {
@@ -109,7 +80,6 @@ std::list<Number> _GetSolution(char* szTelephoneNumber, int32_t i32NumbersCount,
         if ((i32TelephoneNumberSize >= pNumbers[i].m_i32NumberSize) &&
             !strncmp(szTelephoneNumber, pNumbers[i].m_szNumber, pNumbers[i].m_i32NumberSize))
         {
-            pNumbers[i].m_bUsed = true;
             bool result = _RecursiveGetSolution(
                 szTelephoneNumber + pNumbers[i].m_i32NumberSize,
                 i32TelephoneNumberSize - pNumbers[i].m_i32NumberSize,
@@ -121,12 +91,32 @@ std::list<Number> _GetSolution(char* szTelephoneNumber, int32_t i32NumbersCount,
             {
                 if (lMinNumbers.empty() || lMinNumbers.size() > lTmpNumbers.size())
                 {
-                    lMinNumbers = lTmpNumbers;
+                    lMinNumbers = std::move(lTmpNumbers);
+                    lMinNumbers.push_back(pNumbers[i]);
+                    if (1 == lMinNumbers.size())
+                    {
+                        break;
+                    }
                 }
             }
-            pNumbers[i].m_bUsed = false;
         }
     }
+    bool result = !lMinNumbers.empty();
+    if (result)
+    {
+        lNumbers.splice(lNumbers.end(), lMinNumbers);
+    }
+    return result;
+}
+
+std::list<Number> _GetSolution(char* szTelephoneNumber, int32_t i32NumbersCount, Number* pNumbers)
+{
+    static Number noSolution = { 12, "No solution.", "No solution." };
+
+    int32_t i32TelephoneNumberSize = strlen(szTelephoneNumber);
+
+    std::list<Number> lMinNumbers;
+    _RecursiveGetSolution(szTelephoneNumber, i32TelephoneNumberSize, i32NumbersCount, pNumbers, lMinNumbers);
     if (lMinNumbers.empty())
     {
         lMinNumbers.push_back(noSolution);
@@ -139,34 +129,43 @@ int32_t Solve(FILE *in, FILE *out)
     _FormLettersNumbersMap();
 
     char szTelephoneNumber[101] = { 0 };
-    fscanf_s(in, "%s", &szTelephoneNumber);
+    fscanf(in, "%s", szTelephoneNumber);
 
-    while (strncmp(szTelephoneNumber, "-1", sizeof(szTelephoneNumber)))
+    while (strncmp(szTelephoneNumber, "-1", sizeof("-1")))
     {
         int32_t i32WordsCount = 0;
-        fscanf_s(in, "%d", &i32WordsCount);
+        fscanf(in, "%d", &i32WordsCount);
 
         char word[51];
         Number* pNumbers = new Number[i32WordsCount];
 
         for (int32_t i = 0; i < i32WordsCount; ++i)
         {
-            fscanf_s(in, "%s", &word);
+            fscanf(in, "%s", word);
             _FormNumber(word, pNumbers[i]);
-            pNumbers[i].m_bUsed = false;
         }
 
         // TODO: solve
-
+        std::list<Number> lNumbers = _GetSolution(szTelephoneNumber, i32WordsCount, pNumbers);
+        bool first = true;
+        for (auto numberIt = lNumbers.rbegin(); numberIt != lNumbers.rend(); ++numberIt)
+        {
+            if (!first)
+                fprintf_s(out, " ");
+            fprintf_s(out, "%s", numberIt->m_szWord);
+            first = false;
+        }
+        fprintf_s(out, "\n");
 
         for (int32_t i = 0; i < i32WordsCount; ++i)
         {
+            delete[] pNumbers[i].m_szWord;
             delete[] pNumbers[i].m_szNumber;
         }
         delete[] pNumbers;
 
         // get telephone number
-        fscanf_s(in, "%s", &szTelephoneNumber);
+        fscanf(in, "%s", &szTelephoneNumber);
     }
 
     return -1;
--- timus/timus/main.cpp
+++ timus/timus/main.cpp
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <cstdint>
+#include "1002.h"
 
 int32_t main(int32_t argc, char* argv[])
 {
+    FILE* in = fopen("in.txt", "r");
+    FILE* out = fopen("out.txt", "w");
+    Solve(in, out);
     return 0;
 }
 

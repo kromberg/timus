56c3df603dfd8aedd04b218f9170ae1be20eab66
kromberg <pomidorka.kromberg@gmail.com>
2017-04-11 09:27:29 +0300

    1002 solution

--- timus/1002.cpp
+++ timus/1002.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <vector>
 #include <list>
+#include <unordered_set>
 
 #include "1002.h"
 
@@ -48,33 +49,47 @@ char mapping(char c)
 
 bool getWords(
     std::list<std::vector<WordNumber>::const_iterator>& result,
+    std::unordered_set<const WordNumber*>& numbersUsed,
     const std::string& number,
     size_t position,
     const std::vector<WordNumber>& wordNumbers)
 {
+    std::cout << "Number: " << number << "; position: " << position << '\n';
     if (number.size() == position)
     {
+        std::cout << "The end of the number is reached" << '\n';
         return true;
     }
     std::string c(1, number[position]);
     auto it = std::lower_bound(wordNumbers.begin(), wordNumbers.end(), c);
     if (wordNumbers.end() == it)
     {
+        std::cout << "Lower bound is not found" << '\n';
         return false;
     }
-    while (it->m_number[0] == c[0])
+    std::cout << "Lower bound: " << it->m_number << "; c[0] = " << c[0] << '\n';
+    while (wordNumbers.end() != it && it->m_number[0] == c[0])
     {
-        if (number.compare(position, it->m_number.size(), it->m_number))
+        auto numberIt = numbersUsed.find(&(*it));
+        if (numbersUsed.end() != numberIt)
+        {
+            ++ it;
+            continue;
+        }
+        if (!number.compare(position, it->m_number.size(), it->m_number))
         {
             std::list<std::vector<WordNumber>::const_iterator> tmpResult;
-            if (getWords(tmpResult, number, position + it->m_number.size(), wordNumbers))
+            numbersUsed.insert(&(*it));
+            if (getWords(tmpResult, numbersUsed, number, position + it->m_number.size(), wordNumbers))
             {
                 tmpResult.push_front(it);
+                std::cout << "Adding " << it->m_word << '\n';
                 if (result.empty() || tmpResult.size() < result.size())
                 {
                     result = std::move(tmpResult);
                 }
             }
+            numbersUsed.erase(&(*it));
         }
 
         ++ it;
@@ -82,6 +97,7 @@ bool getWords(
 
     if (result.empty())
     {
+        std::cout << "Result is empty" << '\n';
         return false;
     }
 
@@ -103,13 +119,14 @@ void solve(std::istream& in, std::ostream& out)
             std::transform(
                 wordNumbers[i].m_word.begin(), wordNumbers[i].m_word.end(),
                 std::back_inserter(wordNumbers[i].m_number), mapping);
-            std::cout << wordNumbers[i].m_word << " -> " << wordNumbers[i].m_number << '\n';
+            //std::cout << wordNumbers[i].m_word << " -> " << wordNumbers[i].m_number << '\n';
         }
         std::sort(wordNumbers.begin(), wordNumbers.end());
 
         std::list<std::vector<WordNumber>::const_iterator> res;
+        std::unordered_set<const WordNumber*> numbersUsed;
 
-        if (getWords(res, number, 0, wordNumbers))
+        if (getWords(res, numbersUsed, number, 0, wordNumbers))
         {
             for (const auto& it : res)
             {
